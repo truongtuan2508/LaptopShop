16fe91473b71f71ef077c098da3ec7022c4c8b5c
truongtuan2508 <truongtuan2508@gmail.com>
2025-09-23 20:14:50 +0700

    Add admin dashboard, order, product

---
+++ src/main/java/vn/hiplatui/laptopshop/controller/admin/DashboardController.java
@@ -0,0 +1,14 @@
+package vn.hiplatui.laptopshop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/admin")
+    public String getDashboard() {
+        return "admin/dashboard/admin";
+    }
+
+}
