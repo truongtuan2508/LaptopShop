c360d4dee3a4e07b28caa72e7060f542c0f40e08
truongtuan2508 <truongtuan2508@gmail.com>
2025-09-25 15:13:50 +0700

    Update create product

--- src/main/java/vn/hiplatui/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/hiplatui/laptopshop/controller/admin/ProductController.java
@@ -1,14 +1,23 @@
 package vn.hiplatui.laptopshop.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import vn.hiplatui.laptopshop.domain.Product;
 
 @Controller
 public class ProductController {
 
     @GetMapping("/admin/product")
-    public String getDashboard() {
-        return "admin/product/product";
+    public String getProduct() {
+        return "admin/product/index";
+    }
+
+    @GetMapping("admin/product/create") // Get
+    public String getCreateProductPage(Model model) {
+        model.addAttribute("newProduct", new Product());
+        return "admin/product/create";
     }
 
 }
--- src/main/java/vn/hiplatui/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hiplatui/laptopshop/controller/client/ItemController.java
@@ -3,7 +3,13 @@ package vn.hiplatui.laptopshop.controller.client;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import vn.hiplatui.laptopshop.domain.User;
 
 @Controller
 public class ItemController {
@@ -16,4 +22,5 @@ public class ItemController {
     public String getHomePageTest() {
         return "client/product/detail";
     }
+
 }
