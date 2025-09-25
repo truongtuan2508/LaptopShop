71a2ff1ea8878af53081db0d0748bc89edf0a0f4
truongtuan2508 <truongtuan2508@gmail.com>
2025-09-25 13:00:15 +0700

    Add homepage

--- src/main/java/vn/hiplatui/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hiplatui/laptopshop/controller/admin/UserController.java
@@ -2,7 +2,6 @@ package vn.hiplatui.laptopshop.controller.admin;
 
 import java.util.List;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/vn/hiplatui/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hiplatui/laptopshop/repository/UserRepository.java
@@ -3,7 +3,6 @@ package vn.hiplatui.laptopshop.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import vn.hiplatui.laptopshop.domain.User;
