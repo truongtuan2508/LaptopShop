7b35e8a7c12e4dfd9fbcef32da28cc9ebd9b88f7
truongtuan2508 <truongtuan2508@gmail.com>
2025-09-25 13:42:38 +0700

    Update product detail

---
+++ src/main/java/vn/hiplatui/laptopshop/controller/client/ItemController.java
@@ -0,0 +1,19 @@
+package vn.hiplatui.laptopshop.controller.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ItemController {
+    @GetMapping("/product/{id}")
+    public String getProductPage(Model model, @PathVariable long id) {
+        return "client/product/detail";
+    }
+
+    @GetMapping("/product")
+    public String getHomePageTest() {
+        return "client/product/detail";
+    }
+}
