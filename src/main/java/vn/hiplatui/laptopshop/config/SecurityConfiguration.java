6f5c4086848632e4df21f547d16a3176ea47649e
truongtuan2508 <truongtuan2508@gmail.com>
2025-09-26 16:23:44 +0700

    Custom logingage spring security

--- src/main/java/vn/hiplatui/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hiplatui/laptopshop/config/SecurityConfiguration.java
@@ -2,15 +2,15 @@ package vn.hiplatui.laptopshop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+import jakarta.servlet.DispatcherType;
 import vn.hiplatui.laptopshop.service.UserService;
 import vn.hiplatui.laptopshop.service.validator.CustomUserDetailsService;
 
@@ -37,6 +37,26 @@ public class SecurityConfiguration {
         authProvider.setPasswordEncoder(passwordEncoder);
         // authProvider.setHideUserNotFoundExceptions(false);
         return authProvider;
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD,
+                                DispatcherType.INCLUDE)
+                        .permitAll()
+
+                        .requestMatchers("/", "login", "/client/**", "/css/**", "/js/**",
+                                "/images/**")
+                        .permitAll()
+
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .failureUrl("/login?error")
+                        .permitAll());
+        return http.build();
     }
 
 }
