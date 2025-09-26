07c7869a8fe5fa6e19a00904cb75e3b22bb2c4be
truongtuan2508 <truongtuan2508@gmail.com>
2025-09-26 15:53:33 +0700

    Fix bug Malicious String //

--- src/main/java/vn/hiplatui/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/hiplatui/laptopshop/controller/admin/ProductController.java
@@ -47,7 +47,7 @@ public class ProductController {
 
         // Validate
         if (newProductBindingResult.hasErrors()) {
-            return "/admin/product/create";
+            return "admin/product/create";
         }
 
         // upload file
