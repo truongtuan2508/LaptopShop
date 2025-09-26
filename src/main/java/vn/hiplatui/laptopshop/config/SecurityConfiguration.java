d3797ec0ef408946bd44a4acdcdabeac268e66ae
truongtuan2508 <truongtuan2508@gmail.com>
2025-09-26 16:55:23 +0700

    Authorize by Role

--- src/main/java/vn/hiplatui/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hiplatui/laptopshop/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import jakarta.servlet.DispatcherType;
 import vn.hiplatui.laptopshop.service.UserService;
@@ -40,6 +41,11 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public AuthenticationSuccessHandler CustomSuccessHandler() {
+        return new CustomSuccessHandler();
+    }
+
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
@@ -47,14 +53,16 @@ public class SecurityConfiguration {
                                 DispatcherType.INCLUDE)
                         .permitAll()
 
-                        .requestMatchers("/", "login", "/client/**", "/css/**", "/js/**",
+                        .requestMatchers("/", "login", "register", "/product/**", "/client/**", "/css/**", "/js/**",
                                 "/images/**")
                         .permitAll()
-
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
+
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
                         .failureUrl("/login?error")
+                        .successHandler(CustomSuccessHandler())
                         .permitAll());
         return http.build();
     }
