f6617c5e424b47cf3d4a9d904f5b3ea1cda78769
truongtuan2508 <truongtuan2508@gmail.com>
2025-09-27 17:42:43 +0700

    Fix bug create new cart

--- src/main/java/vn/hiplatui/laptopshop/service/ProductService.java
+++ src/main/java/vn/hiplatui/laptopshop/service/ProductService.java
@@ -46,10 +46,10 @@ public class ProductService {
 
     public void handleAddProductToCart(String email, long productId) {
         User user = this.userService.getUserByEmail(email);
-        if (user == null) {
+        if (user != null) {
             // check user da co cart chua? Neu chua => tao. moi
             Cart cart = this.cartRepository.findByUser(user);
-            if (cart != null) {
+            if (cart == null) {
                 // Tao moi cart
                 Cart otherCart = new Cart();
                 otherCart.setUser(user);
