1e8d1c32a8401673ce22ec8cf592fa4d525513ac
truongtuan2508 <truongtuan2508@gmail.com>
2025-09-27 19:03:45 +0700

    Load data cart

--- src/main/java/vn/hiplatui/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hiplatui/laptopshop/controller/client/ItemController.java
@@ -1,11 +1,16 @@
 package vn.hiplatui.laptopshop.controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hiplatui.laptopshop.domain.Cart;
+import vn.hiplatui.laptopshop.domain.CartDetail;
 import vn.hiplatui.laptopshop.domain.Product;
+import vn.hiplatui.laptopshop.domain.User;
 import vn.hiplatui.laptopshop.service.ProductService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,7 +46,24 @@ public class ItemController {
     }
 
     @GetMapping("/cart")
-    public String getCartPage(Model model) {
+    public String getCartPage(Model model, HttpServletRequest request) {
+        User currentUser = new User();
+        HttpSession session = request.getSession(false);
+        long id = (long) session.getAttribute("id");
+        currentUser.setId(id);
+
+        Cart cart = this.productService.fetchByUser(currentUser);
+
+        List<CartDetail> cartDetails = cart.getCartDetails();
+
+        double totalPrice = 0;
+        for (CartDetail cd : cartDetails) {
+            totalPrice += cd.getPrice() * cd.getQuantity();
+        }
+
+        model.addAttribute("cartDetails", cartDetails);
+        model.addAttribute("totalPrice", totalPrice);
+
         return "client/cart/index";
     }
 
--- src/main/java/vn/hiplatui/laptopshop/service/ProductService.java
+++ src/main/java/vn/hiplatui/laptopshop/service/ProductService.java
@@ -93,4 +93,8 @@ public class ProductService {
         }
 
     }
+
+    public Cart fetchByUser(User user) {
+        return this.cartRepository.findByUser(user);
+    }
 }
